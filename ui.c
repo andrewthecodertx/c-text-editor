ebf2d096945a58b30a17e6fc1e5030df93bbdca2
Andrew S Erwin <andrewerwin73@gmail.com>
2026-07-27 20:43:11 -0500

    style: clang-format ui.c after rebase

--- ui.c
+++ ui.c
@@ -4,8 +4,8 @@
 
 #include <ncurses.h>
 #include <stdarg.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 #include "ui_constants.h"
@@ -15,7 +15,7 @@ time_t status_message_time;
 
 void editor_draw_rows()
 {
-    EditorConfig *E = get_editor_config();
+    EditorConfig* E = get_editor_config();
     int y;
     for (y = 0; y < E->screen_rows; y++)
     {
@@ -26,7 +26,7 @@ void editor_draw_rows()
         }
         else
         {
-            EditorLine *line = &E->lines.elements[filerow];
+            EditorLine* line = &E->lines.elements[filerow];
             int current_color_pair = HL_NORMAL;
             int display_col = 0;
 
@@ -82,7 +82,7 @@ void editor_draw_rows()
 
 void editor_draw_status_bar()
 {
-    EditorConfig *E = get_editor_config();
+    EditorConfig* E = get_editor_config();
     attron(A_REVERSE);
 
     mvprintw(E->screen_rows, 0, "%.20s - %d lines %s", E->filename ? E->filename : "[No Name]",
@@ -95,7 +95,7 @@ void editor_draw_status_bar()
     attroff(A_REVERSE);
 }
 
-void editor_set_status_message(const char *fmt, ...)
+void editor_set_status_message(const char* fmt, ...)
 {
     va_list ap;
     va_start(ap, fmt);
@@ -106,7 +106,7 @@ void editor_set_status_message(const char *fmt, ...)
 
 void editor_draw_message_bar()
 {
-    EditorConfig *E = get_editor_config();
+    EditorConfig* E = get_editor_config();
     move(E->screen_rows + 1, 0);
     clrtoeol();
 
@@ -121,9 +121,9 @@ void editor_draw_message_bar()
 
 void editor_draw_clock()
 {
-    EditorConfig *E = get_editor_config();
+    EditorConfig* E = get_editor_config();
     time_t rawtime;
-    struct tm *info;
+    struct tm* info;
     char time_str[6];
 
     time(&rawtime);
@@ -139,7 +139,7 @@ void editor_draw_clock()
 
 void editor_scroll()
 {
-    EditorConfig *E = get_editor_config();
+    EditorConfig* E = get_editor_config();
     if (E->cy < E->row_offset)
     {
         E->row_offset = E->cy;
@@ -169,7 +169,7 @@ void editor_scroll()
 
 void editor_refresh_screen()
 {
-    EditorConfig *E = get_editor_config();
+    EditorConfig* E = get_editor_config();
     editor_scroll();
 
     clear();
@@ -185,15 +185,15 @@ void editor_refresh_screen()
 
 int get_cx_display()
 {
-    EditorConfig *E = get_editor_config();
+    EditorConfig* E = get_editor_config();
     int display_cx = 0;
     if (E->cy >= E->lines.size)
         return 0;
 
-    EditorLine *line = &E->lines.elements[E->cy];
+    EditorLine* line = &E->lines.elements[E->cy];
     for (int i = 0; i < E->cx; i++)
     {
-        if ((size_t)i >= line->len)
+        if ((size_t) i >= line->len)
             break;
         if (line->text[i] == '\t')
         {
@@ -207,10 +207,10 @@ int get_cx_display()
     return display_cx;
 }
 
-char *editor_prompt(const char *prompt_fmt, ...)
+char* editor_prompt(const char* prompt_fmt, ...)
 {
     size_t capacity = 256;
-    char *buffer = malloc(capacity);
+    char* buffer = malloc(capacity);
     int buflen = 0;
 
     if (buffer == NULL)
@@ -252,10 +252,10 @@ char *editor_prompt(const char *prompt_fmt, ...)
         }
         else if (c >= 32 && c <= 126)
         {
-            if ((size_t)buflen + 1 >= capacity)
+            if ((size_t) buflen + 1 >= capacity)
             {
                 size_t new_capacity = capacity * 2;
-                char *grown = realloc(buffer, new_capacity);
+                char* grown = realloc(buffer, new_capacity);
                 if (grown == NULL)
                 {
                     free(buffer);
@@ -265,7 +265,7 @@ char *editor_prompt(const char *prompt_fmt, ...)
                 buffer = grown;
                 capacity = new_capacity;
             }
-            buffer[buflen++] = (char)c;
+            buffer[buflen++] = (char) c;
             buffer[buflen] = '\0';
         }
     }
@@ -273,8 +273,8 @@ char *editor_prompt(const char *prompt_fmt, ...)
 
 void handle_winch(int sig)
 {
-    EditorConfig *E = get_editor_config();
-    (void)sig;
+    EditorConfig* E = get_editor_config();
+    (void) sig;
     endwin();
     refresh();
     getmaxyx(stdscr, E->screen_rows, E->screen_cols);
